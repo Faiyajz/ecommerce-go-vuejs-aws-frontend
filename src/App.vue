87a43316899c38ed9361a388ca9b3adda1a3ddcc
faiyaj <faiyajz007@gmail.com>
2022-12-05 15:18:49 +0600

    Homepage Carousel added, bootstrap grid covered

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,40 @@
 <template>
   <NavBar></NavBar>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 d-flex justify-content-center">
+        <CarouselMain></CarouselMain>
+      </div>
+    </div>
+  </div>
+
+  <div class="container text-start">
+    <div class="row border border-1">
+      <div class="col-6 col-md-2 border">
+        text-1
+      </div>
+      <div class="col-6 col-md-8 border">
+        text-2
+      </div>
+      <div class="col-12 col-md-2">
+        text-3
+      </div>
+
+    </div>
+
+  </div>
+
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
+import CarouselMain from './components/CarouselMain.vue'
 
 export default {
   name: 'App',
   components: {
     NavBar,
+    CarouselMain,
     
   }
 }
